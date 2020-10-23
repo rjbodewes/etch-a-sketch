022d98e1015dabe1afcb87e07d9ba17fcc87c1ba
rjbodewes <rjbodewes@gmail.com>
2020-10-23 15:14:58 +0200

    just an update

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
-const size = 16
+
+function convertSize(x) {
+    if (x == 1) {
+        return 4;
+    } else if (x == 2) {
+        return 16; 
+    } else if (x == 3) {
+        return 32;
+    } else {
+        return 64;
+    }
+}
+
+
+/*const size = 16*/
 const container = document.querySelector("#container");
 
 
 
 
-for (i = 1; i <= size; i++) {
-    const cell = document.createElement("div"); 
-    cell.innerHTML = `${i}`;
-    container.appendChild(cell);
-};s
+
+
+var slider = document.getElementById("mySlider");
+
+
+
+
+    
+
+slider.addEventListener("input", function() {
+
+    var node = document.getElementById('mySlider');
+    node.innerHTML = "";
+
+    var guideValue = event.target.value;
+    console.log(guideValue);
+    
+    for (i = 1; i <= convertSize(guideValue); i++) {
+        const cell = document.createElement("div"); 
+        cell.innerHTML = ``;
+        container.appendChild(cell);
+    };
+
+
+
+});
+
